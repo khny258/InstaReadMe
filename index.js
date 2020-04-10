89056e0c4271a9ee999857c3f1202a5d8688c76a
Kenny Huang <khny258@gmail.com>
2020-04-10 13:54:06 -0400

    added rest of generator

--- index.js
+++ index.js
@@ -79,3 +79,71 @@ inquirer.prompt([
     })   
 });
 
+
+function getData({username, project, description, licensetype, dependencies, tests, about, contributing},userImage) {
+
+    console.log("project",project);
+    return `
+# ${project}
+
+## Description
+
+${description}
+        
+## Table of Contents
+        
+* [Installation](#installation)
+        
+* [Usage](#usage)
+        
+* [License](#license)
+        
+* [Contributing](#contributing)
+        
+* [Tests](#tests)
+        
+* [Questions](#questions)
+        
+## Installation
+        
+To install necessary dependencies, run the following command:
+        
+\`\`\`
+${dependencies}
+\`\`\`
+        
+## Usage
+
+${about}
+        
+## License
+
+${licensetype}
+        
+## Contributing
+        
+${contributing}
+
+## Tests
+        
+To run tests, run the following command:
+        
+\`\`\`
+${tests}
+\`\`\`
+        
+## Questions
+
+![user profile image](${userImage})
+        
+If you have any questions about the repo, open an issue or contact [${username}](https://api.github.com/users/${username}) directly at null.
+        
+`;
+
+}
+
+    module.exports = getData;
+
+//function init() {}
+
+//init();
